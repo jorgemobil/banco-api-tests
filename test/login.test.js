5a3475a824318b4709f64be675371243d1791ca7
Bowie <marketing@letscode.com.br>
2025-10-31 17:07:26 -0300

    Adicionando fixtures para gerir corpo das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -1,19 +1,17 @@
 const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json')
 
 //estou usando o async por causa do await se eu nao usar o await não preciso do async
-
 describe('Login',  () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com token em string quando usar credenciais válidas ', async() => {
+            const bodyLogin = { ...postLogin }
             const resposta = await request(process.env.BASE_URL)
             .post('/login')
             .set('Content-Type', 'application/json')
-            .send({
-                'username': 'julio.lima',
-                'senha': '123456'
-            })
+            .send(postLogin)
 
             console.log(resposta.status)
             console.log(resposta.body)
